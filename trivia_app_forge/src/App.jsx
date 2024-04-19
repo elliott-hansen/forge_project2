757bc8c0b2419e83166350084516e8e441b6f36e
elliott-hansen <enh4bn@virginia.edu>
2024-04-19 17:46:16 -0400

    changes

--- trivia_app_forge/src/App.jsx
+++ trivia_app_forge/src/App.jsx
@@ -2,8 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider  } from '@mui/material/styles';
+
 import './App.css'
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#1f1f1f"
+        }
+    }
+})
 
 // async call to api, returns 'questions' object containing 10 generated questions.
 var url = "https://the-trivia-api.com/v2/questions";
@@ -25,14 +34,20 @@ function QuestionBox(question) {
     const question_head = question.question.question.text; // wtf?
     const incorrect_choices = question.question.incorrectAnswers;
     const correct_choice = question.question.correctAnswer;
-    var total_choices = incorrect_choices.map((choice) => toChoiceObj(choice, false));
-    total_choices.push(toChoiceObj(correct_choice, true))
-    // Shuffle choices, Schwartzian Transform from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-    total_choices = total_choices
-        .map(value => ({value, sort: Math.random()}))
-        .sort((a,b) => a.sort - b.sort)
-        .map (({value}) => value)
-    console.log(total_choices);
+    if (!total_choices) {
+        var total_choices = incorrect_choices.map((choice) => toChoiceObj(choice, false));
+        total_choices.push(toChoiceObj(correct_choice, true))
+        // Shuffle choices, Schwartzian Transform from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+        total_choices = total_choices
+            .map(value => ({value, sort: Math.random()}))
+            .sort((a,b) => a.sort - b.sort)
+            .map (({value}) => value)
+    }
+    // const [style, setStyle] = useState("question-box-main")
+    
+    // function toggler() {
+    //     console.log("TOGGLE");
+    // }
     return (
         <>
             <div className="question-box-main">
@@ -47,30 +62,31 @@ function QuestionBox(question) {
     )
 }
 
-function QuestionChoice(choice) {
-    var isCorrect = choice.choice.isCorrect
+const QuestionChoice = ({choice}) => {
+    var isCorrect = choice.isCorrect
     const [clicked, setClicked] = useState(false);
-    const handleClick = () => {
+    function handleClick() {
         setClicked(!clicked);
     }
     const statusColor = isCorrect? "success" : "error";
     return (
         <>
             <div className="choice-box">
-                <Button 
-                    variant='text'
-                    onClick={handleClick}
-                    color={clicked? statusColor : "primary"}
-                >
-                    {choice.choice.choiceText}
-                </Button>
+                <ThemeProvider theme={theme}>
+                    <Button 
+                        variant='contained'
+                        onClick={handleClick}
+                        color={clicked? statusColor : "primary"}
+                    >
+                        {choice.choiceText}
+                    </Button>
+                </ThemeProvider>
             </div>
         </>
     )
 }
 
 function App() {
-  console.log(questions);
   return (
     <>
         {questions.map((question, i) => <QuestionBox question={question} key={i}/>)}
